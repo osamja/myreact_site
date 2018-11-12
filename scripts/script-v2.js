0eecb34f931f7b2bb9a247a7a4edc9efdbdcf13b
Sammy Javed <sammyjaved@gmail.com>
2018-11-11 18:14:31 -0800

    need to study code

--- scripts/script-v2.js
+++ scripts/script-v2.js
@@ -17,22 +17,26 @@ class Gallery extends React.Component {
         this.findPrev = this.findPrev.bind(this);
         this.renderImageContent = this.renderImageContent.bind(this);
     }
+
     renderImageContent(src, index) {
         return (
             <div onClick={(e) => this.openModal(e, index)}>
-    <img src={src} key={src} />
-        </div>
-    )
+            <img src={src} key={src} />
+            </div>
+        )
     }
+
     openModal(e, index) {
         this.setState ({ currentIndex: index });
     }
+
     closeModal(e) {
         if (e !== undefined) {
             e.preventDefault();
         }
         this.setState ({ currentIndex: null });
     }
+
     findPrev(e) {
         if (e !== undefined) {
             e.preventDefault();
@@ -41,6 +45,7 @@ class Gallery extends React.Component {
             currentIndex: prevState.currentIndex -1
         }));
     }
+
     findNext(e) {
         if (e !== undefined) {
             e.preventDefault();
@@ -49,23 +54,24 @@ class Gallery extends React.Component {
             currentIndex: prevState.currentIndex + 1
         }));
     }
+
     render() {
         return (
             <div className="gallery-container">
             <h1>🔥 Sammy Javed 🔥</h1>
-        <div className="gallery-grid">
-            {imgUrls.map(this.renderImageContent)}
-    </div>
-        <GalleryModal
-        closeModal={this.closeModal}
-        findPrev={this.findPrev}
-        findNext={this.findNext}
-        hasPrev={this.state.currentIndex > 0}
-        hasNext={this.state.currentIndex + 1 < imgUrls.length}
-        src={imgUrls[this.state.currentIndex]}
-        />
-        </div>
-    )
+            <div className="gallery-grid">
+                {imgUrls.map(this.renderImageContent)}
+            </div>
+            <GalleryModal
+                closeModal={this.closeModal}
+                findPrev={this.findPrev}
+                findNext={this.findNext}
+                hasPrev={this.state.currentIndex > 0}
+                hasNext={this.state.currentIndex + 1 < imgUrls.length}
+                src={imgUrls[this.state.currentIndex]}
+            />
+            </div>
+        )
     }
 }
 
@@ -74,12 +80,15 @@ class GalleryModal extends React.Component {
         super();
         this.handleKeyDown = this.handleKeyDown.bind(this);
     }
+
     componentDidMount() {
         document.body.addEventListener('keydown', this.handleKeyDown);
     }
+
     componentWillUnMount() {
         document.body.removeEventListener('keydown', this.handleKeyDown);
     }
+
     handleKeyDown(e) {
         if (e.keyCode === 27)
             this.props.closeModal();
@@ -88,6 +97,7 @@ class GalleryModal extends React.Component {
         if (e.keyCode === 39 && this.props.hasNext)
             this.props.findNext();
     }
+
     render () {
         const { closeModal, hasNext, hasPrev, findNext, findPrev, src } = this.props;
         if (!src) {
@@ -100,13 +110,13 @@ class GalleryModal extends React.Component {
             <div isOpen={!!src} className="modal">
             <div className='modal-body'>
             <a href="#" className='modal-close' onClick={closeModal} onKeyDown={this.handleKeyDown}>&times;</a>
-        {hasPrev && <a href="#" className='modal-prev' onClick={findPrev} onKeyDown={this.handleKeyDown}>&lsaquo;</a>}
+            {hasPrev && <a href="#" className='modal-prev' onClick={findPrev} onKeyDown={this.handleKeyDown}>&lsaquo;</a>}
             {hasNext && <a href="#" className='modal-next' onClick={findNext} onKeyDown={this.handleKeyDown}>&rsaquo;</a>}
             <img src={src} />
             </div>
             </div>
             </div>
-            )
+        )
     }
 }
 
